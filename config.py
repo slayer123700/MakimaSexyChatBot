2dbe28a209308610a4f36da2198fbd19c4f30ed7
slayer <meerutuniversity996@gmail.com>
2025-11-06 11:30:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ API_ID = int(getenv("API_ID", "27469215"))
 API_HASH = getenv("API_HASH", "544b135cbf33923fed1a7c461cc95f18")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "8336737769:AAGKtMwq9AomEdD3FTad960azRbDQRWMGQs")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-OWNER_ID = int(getenv("OWNER_ID", "7218905176"))
+OWNER_ID = int(getenv("OWNER_ID", "6018803920"))
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://musicbotxd:musicbotxd@cluster0.6thyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
 SUPPORT_GRP = getenv("SUPPORT_GRP", "Sukunaxupdatess")
